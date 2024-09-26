1a00dfa39eba650f76eb007372e1de379bfcf3e6
Ivan Butryn <ivanbutryn@MacBook-Air-Ivan.local>
2024-09-26 17:00:56 +0300

    Added acordion + css + js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', function() {
+    this.classList.toggle('active');
+
+    const content = this.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+});
